23d6511e2a39ce62b9c5e1bd9778e92628f9a55b
Jabed-Hasan <badboy07312@gmail.com>
2024-11-17 18:17:10 +0600

    Problem-7 Solved

---
+++ modules/src/problem-7.ts
@@ -0,0 +1,21 @@
+class Car {
+    make: string;
+    model: string;
+    year: number;
+  
+    constructor(make: string, model: string, year: number) {
+      this.make = make;
+      this.model = model;
+      this.year = year;
+    }
+  
+    getCarAge(): void {
+      const currentYear = 2024;  
+      const age = currentYear - this.year;
+      console.log(`${age} (assuming current year is ${currentYear})`);
+    }
+  }
+  
+  const car = new Car("Honda", "Civic", 2018);
+  car.getCarAge(); 
+  
