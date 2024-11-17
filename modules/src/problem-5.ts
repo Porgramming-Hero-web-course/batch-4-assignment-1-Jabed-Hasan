f130f6012f3b981c7ec5b781228914177eea9a59
Jabed-Hasan <badboy07312@gmail.com>
2024-11-18 00:12:22 +0600

    Problem-5 updated

--- modules/src/problem-5.ts
+++ modules/src/problem-5.ts
@@ -6,4 +6,4 @@ function getProperty<T, K extends keyof T>(obj: T, key: K): T[K] {
 // Sample Input
 const person = { name: "Alice", age: 30 };
 console.log(getProperty(person, "name")); 
-console.log(getProperty(person, "age"));  
+
